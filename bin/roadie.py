77e023a804969295bf601a854f8780815fdfcb39
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2015-11-06 18:10:44 +0900

    Add upload results and garbage collection.

--- bin/roadie.py
+++ bin/roadie.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import glob
 import os
 import subprocess
 import sys
@@ -8,30 +9,71 @@ from downloader import download
 
 
 TEMPPATH = "/tmp/stdout{0}.txt"
-def run(command, step=1):
+def execute(command, stdout, stderr=sys.stdout):
     """ Run a given command.
 
     Args:
       command: a string specifying command to be run.
-      step: step number used for files saving stdout.
+      stdout: Writable object to which stdout of subprocess connects.
+      stderr: Writable object to which stderr of subprocess connects.
     """
     # Does tail work to watch stdout to logging service?
-    with open(TEMPPATH.format(step), "w") as stdout:
-        p = subprocess.Popen(
-            command, shell=True, stdout=stdout, stderr=sys.stdout)
-        p.wait()
+    p = subprocess.Popen(
+        command, shell=True, stdout=stdout, stderr=stderr)
+    p.wait()
 
 
-def main():
-    with open("test.yml") as fp:
-        obj = yaml.load(fp)
-        print obj
+def upload(pat, dest):
+    """ Upload objects matched to a given pattern.
 
+    Args:
+      pat: Pattern of objects to be uploaded.
+      dest: Destination URL.
+    """
+    p = subprocess.Popen(
+        ["gsutil", "cp", pat, dest], stdout=sys.stdout)
+    p.wait()
+
+
+# whether runner should stop when getting status codes not 0?
+def run(conf):
+    """ Run.
+
+    Args:
+      conf: Redable object consists of conf file.
+    """
+    obj = yaml.load(conf)
+
+    # Prepare data.
+    if "data" in obj:
         for url in obj["data"]:
             download(url)
 
-        for i, com in enumerate(obj["run"]):
-            run(com, i + 1)
+    # Run command.
+    for i, com in enumerate(obj["run"]):
+        with open(TEMPPATH.format(i), "w") as fp:
+            execute(com, fp)
+
+    # Upload results.
+    dest = obj["result"]["destination"]
+    upload(TEMPPATH.format("*"), dest)
+    for pat in obj["result"]["pattern"]:
+        upload(pat, dest)
+
+    # Garbage collection.
+    for path in glob.iglob(TEMPPATH.format("*")):
+        os.remove(path)
+
+
+def main():
+    """ The main function.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "conf", nargs="+", default=sys.stdin, type=argparse.FileType("w")
+        help="")
+
+    run(**vars(parser.parse_args()))
 
 
 if __name__ == "__main__":
