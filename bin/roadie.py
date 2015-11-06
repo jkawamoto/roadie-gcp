efd9c3dd87e197daab21d22377be482ac317ee54
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2015-11-06 15:46:13 +0900

    Move functions about downloading objects to downloader.py.

--- bin/roadie.py
+++ bin/roadie.py
@@ -2,93 +2,9 @@
 import os
 import subprocess
 import sys
-import urlparse
 import yaml
 
-
-def curl(url, dest):
-    """ Download an object using curl.
-
-    Args:
-      url: Url specifying an object.
-      dest: Destination path.
-
-    Returns:
-      Downloaded filename.
-    """
-    p = subprocess.Popen(
-        ["curl", "-O", urlparse.urlunparse(url)], stdout=sys.stdout, cwd=dest)
-    p.wait()
-
-    return dest + url.path[url.path.rfind("/"):]
-
-
-def gsutil(url, dest):
-    """ Download an object using gsutil.
-
-    Args:
-      url: Url specifying an object.
-      dest: Destination path.
-
-    Returns:
-      Downloaded filename.
-    """
-    p = subprocess.Popen(
-        ["gsutil", "cp", urlparse.urlunparse(url), dest], stdout=sys.stdout)
-    p.wait()
-
-    return dest + url.path[url.path.rfind("/"):]
-
-
-def dropbox(url, dest):
-    """ Download an object from dropbox.
-
-    Args:
-      url: Url specifying an object.
-      dest: Destination path.
-
-    Returns:
-      Downloaded filename.
-    """
-    new_url = "https://{host}{path}?dl=1".format(host=url.netloc, path=url.path)
-    filename = url.path[url.path.rfind("/"):]
-    if filename.find(".") == -1:
-        filename += ".zip"
-    filepath = dest + filename
-    p = subprocess.Popen(
-        ["wget", new_url, "-O", filepath], stdout=sys.stdout)
-    p.wait()
-
-    return filepath
-
-
-def download(url):
-    """ Download an object specified by an url.
-    """
-    # Check the url contains a destination path.
-    dest = "."
-    if url.find(":") != url.rfind(":"):
-        dest = url[url.rfind(":")+1:]
-        url = url[:url.rfind(":")]
-
-    url = urlparse.urlparse(url)
-
-    downloader = curl
-    if url.scheme == "gs":
-        downloader = gsutil
-
-    elif url.scheme == "dropbox":
-        downloader = dropbox
-
-    path = downloader(url, dest)
-
-    # If donloaded file is a zip, unzip and remove it.
-    if path.endswith(".zip"):
-        p = subprocess.Popen(
-            ["unzip", "-o", path], stdout=sys.stdout, cwd=dest)
-        p.wait()
-
-        os.remove(path)
+from downloader import download
 
 
 TEMPPATH = "/tmp/stdout{0}.txt"
@@ -101,18 +17,22 @@ def run(command, step=1):
     """
     # Does tail work to watch stdout to logging service?
     with open(TEMPPATH.format(step), "w") as stdout:
-        print command
         p = subprocess.Popen(
             command, shell=True, stdout=stdout, stderr=sys.stdout)
         p.wait()
 
 
-with open("test.yml") as fp:
-    obj = yaml.load(fp)
-    print obj
+def main():
+    with open("test.yml") as fp:
+        obj = yaml.load(fp)
+        print obj
 
-    for url in obj["data"]:
-        download(url)
+        for url in obj["data"]:
+            download(url)
 
-    for i, com in enumerate(obj["run"]):
-        run(com, i + 1)
+        for i, com in enumerate(obj["run"]):
+            run(com, i + 1)
+
+
+if __name__ == "__main__":
+    main()
